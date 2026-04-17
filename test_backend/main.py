c9468226210c78d639185295f7850de50b3a0ee2
Sravesh <sraveshnandan@gmail.com>
2026-04-17 22:00:12 +0530

    Fix: Disable reload and install package

--- test_backend/main.py
+++ test_backend/main.py
@@ -102,4 +102,4 @@ async def update_config(config: ConfigUpdate):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False, access_log=False)
